5a8b21b0297c30029366e1b8ffc9491f0ef957fb
Rui Mendes <azuki@di.uminho.pt>
2021-11-18 09:47:35 +0000

    Create fetch.py

---
+++ fetch.py
@@ -0,0 +1,7 @@
+alunos = """pg47607;Ricardo da Silva Correia;https://github.com/ricardocorreia99/Trabalhos-de-casa-Programa-o-probabilistica
+pg47474;Maria Inês Machado Correia Brioso Dias;https://github.com/ines-correiadias/Programacao_Probabilistica_FICHAS.git
+pg47446;Márcio Eduardo Lima Mano;https://github.com/mArcio-Lmano/prog_prob2021
+pg47622;Rodrigo da Silva Gomes Peres;Coelho https://github.com/Rodrigelho/Probabilistic-Programming.git
+pg47264;Irving Leander Reascos Valencia;https://github.com/LeanderReascos/probabilistic_programming
+pg45927;Catarina Assis Quintela Madaleno;https://github.com/catarinamadaleno/prog_prob/tree/main/Fichas%20Prog%20Prob
+a89135;Pedro Daniel Lopes Mendes;https://github.com/DanieMendes/Prog_Prob"""
