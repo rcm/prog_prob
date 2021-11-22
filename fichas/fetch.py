a226a9b168ae247e6835465bceb755ee4853ed82
Rui Mendes <azuki@di.uminho.pt>
2021-11-22 13:23:13 +0000

    Corrigi githubs

--- fichas/fetch.py
+++ fichas/fetch.py
@@ -3,9 +3,9 @@ import os
 alunos = """pg47607;Ricardo da Silva Correia;https://github.com/ricardocorreia99/Trabalhos-de-casa-Programa-o-probabilistica
 pg47474;Maria Inês Machado Correia Brioso Dias;https://github.com/ines-correiadias/Programacao_Probabilistica_FICHAS.git
 pg47446;Márcio Eduardo Lima Mano;https://github.com/mArcio-Lmano/prog_prob2021
-pg47622;Rodrigo da Silva Gomes Peres;Coelho https://github.com/Rodrigelho/Probabilistic-Programming.git
+pg47622;Rodrigo da Silva Gomes Peres Coelho;https://github.com/RodrigoCoelho7/Probabilistic-Programming.git
 pg47264;Irving Leander Reascos Valencia;https://github.com/LeanderReascos/probabilistic_programming
-pg45927;Catarina Assis Quintela Madaleno;https://github.com/catarinamadaleno/prog_prob/tree/main/Fichas%20Prog%20Prob
+pg45927;Catarina Assis Quintela Madaleno;https://github.com/catarinamadaleno/prog_prob
 a89135;Pedro Daniel Lopes Mendes;https://github.com/DanieMendes/Prog_Prob
 pg47339;João Pedro Sá Gomes;https://github.com/JoaoPedro9400/Fichas-PP
 pg46949;Alexandre de Jesus Sousa e silva;https://github.com/alexandre04032000/Ficha2
